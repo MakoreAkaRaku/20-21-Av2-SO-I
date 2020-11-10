6487c7589fc9f7b78fa87d376229f2806fbee461
Marc Roman Colom <marcromancolom@gmail.com>
2020-11-10 19:06:00 +0100

    readline(), exec_line() and parse_args() have been made

--- nivel1.c
+++ nivel1.c
@@ -1,5 +1,4 @@
 #include "nivel1.h"
-
 int main(){
     char line[COMMAND_LINE_SIZE]; //1024
     while (1){
@@ -11,12 +10,80 @@ int main(){
     return 1;
 }
 
-char *read_line(char *line){
-    fprintf("%c ",PROMPT);
+/**
+ * Devuelve el puntero de PWD con el string modificado, indicando en que directorio estas.
+ * */
+char *getDirectory(){
+    char *fullPath = getenv("PWD");
+    char result[COMMAND_LINE_SIZE];
+    size_t lenPath = strlen(fullPath);
+    size_t cnt = lenPath;
+    size_t i = 0;
+    --cnt;
+    while (*(fullPath+cnt) != '/' && cnt > 0){
+        --cnt;
+    }
+    if (*(fullPath+cnt) == '/'){
+        cnt++;
+        while (cnt < strlen(fullPath))
+        {
+            result[i] = *(fullPath + cnt);
+            cnt++;
+            i++;
+        }
+        result[i] = '\0';
+        strcpy(fullPath, result);
+    }
+    return fullPath;
+}
+
+/**
+ * Imprime el prompt.
+ * */
+void imprimir_prompt(){
+    char *path = getDirectory();
+    printf("%s»"RESET_COLOR VERDE_T "%s " AZUL_T "%c" RESET_COLOR ": ",execHasMade, path, PROMPT);
     fflush(stdout);
 }
-int execute_line(char *line);
-int parse_args(char **args, char *line);
+/**
+ * Lee la linea de comandos e imprime el prompt.
+ * */
+char *read_line(char *line){
+    imprimir_prompt();
+    fgets(line, COMMAND_LINE_SIZE, stdin);
+    if (line == NULL){ //Looking for condition
+        puts("readline: error al leer el string introducido");
+        return NULL;
+    }
+    return line;
+}
+
+int execute_line(char *line){
+    char *token[COMMAND_LINE_SIZE];
+    int numOfTokens = parse_args(token, line);
+    size_t i = 0;
+    while (*(token+i) != NULL){
+        printf("[%ld]%s\t", i, *(token + i));
+        i++;
+    }
+    printf("[%ld]%s\t", i, *(token + i));
+    //int isinternal = check_internal(token);
+    return 0;
+}
+
+int parse_args(char **args, char *line){
+    int numOfTokens = 0;
+    char *tempToken;
+    tempToken = strtok(line, DELIMITERS);
+    while (tempToken != NULL && *(tempToken) != HASH){
+        *(args + numOfTokens) = tempToken;
+        tempToken = strtok(NULL, DELIMITERS);
+        numOfTokens++;
+    }
+    *(args +numOfTokens) = NULL;
+    return numOfTokens;
+}
+
 int check_internal(char **args);
 int internal_cd(char **args);
 int internal_export(char **args);
--- nivel1.h
+++ nivel1.h
@@ -1,7 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #define COMMAND_LINE_SIZE 1024
 #define PROMPT  '$'
-#define //something
-
+#define HASH    '#'
+#define DELIMITERS " \t\n\r"
+//Constantes para uso de colores
+#define RESET_COLOR "\x1b[0m"
+#define NEGRO_T "\x1b[30m"
+#define NEGRO_F "\x1b[40m"
+#define ROJO_T "\x1b[31m"
+#define ROJO_F "\x1b[41m"
+#define VERDE_T "\x1b[32m"
+#define VERDE_F "\x1b[42m"
+#define AMARILLO_T "\x1b[33m"
+#define AMARILLO_F "\x1b[43m"
+#define AZUL_T "\x1b[34m"
+#define AZUL_F "\x1b[44m"
+#define MAGENTA_T "\x1b[35m"
+#define MAGENTA_F "\x1b[45m"
+#define CYAN_T "\x1b[36m"
+#define CYAN_F "\x1b[46m"
+#define BLANCO_T "\x1b[37m"
+#define BLANCO_F "\x1b[47m"
+char *execHasMade = AMARILLO_T;
 char *read_line(char *line);
 int execute_line(char *line);
 int parse_args(char **args, char *line);
